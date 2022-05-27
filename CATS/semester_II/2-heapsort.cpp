aefaab41e4e496f7f7ffd0f4d36fc6d0f35d9bc9
fs-anvr <94833422+fs-anvr@users.noreply.github.com>
2022-05-27 22:18:15 +1000

    Create 2-heapsort.cpp

---
+++ CATS/semester_II/2-heapsort.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+
+int main()
+{
+    std::ifstream fin ("heapsort.in");
+    std::ofstream fout ("heapsort.out");
+    int number;
+    fin >> number;
+
+    int *unsorted_heap = new int [number + 1];
+    unsorted_heap[1] = 1;
+    int current_number = 1;
+
+    for (int i = 2; i < number + 1; ++i)
+    {
+        for (int j = current_number; j > 1; j /= 2)
+        {
+            unsorted_heap[j] = unsorted_heap[j/2];
+        }
+        unsorted_heap[1] = i;
+        unsorted_heap[++current_number] = 1;
+    }
+    for (int i = 1; i < number + 1; ++i)
+    {
+        fout << unsorted_heap[i] << ' ';
+    }
+    return 0;
+}
+/*
+⠄⠄⠄⠄⣠⣄⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⣼⡟⠉⠉⠄⠄⠄⠄⢀⣀⣀⣀⡀⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⢿⣇⠄⠄⠄⠄⣠⣶⣿⠿⣿⣿⡿⣷⡀⠸⣿⣶⡀⠄⠄
+⠄⠄⠄⠘⢿⣆⠄⣠⣾⣿⣿⣿⣶⣿⣿⣶⣿⠁⠄⣠⣿⡇⠄⠄
+⠄⠄⠄⠄⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢛⣁⣤⣴⣿⠟⠁⠄⠄
+⠄⠄⠄⠄⠄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠁⠄⠄⠄⠄
+⠄⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⣿⣿⡟⠉⠉⠄⠄⠈⣿⣿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⢸⣿⣿⠁⠄⠄⠄⠄⠄⢻⣿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⣾⣿⠇⠄⠄⠄⠄⠄⠄⠄⢿⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠹⢿⠁⡀⠄⠄⠄⠄⠄⠄⠸⣿⣶⡄⠄⠄⠄⠄⠄⠄⠄
+⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠄⠄⠄⠄⠄⠄⠄⠄⠄
+ */
