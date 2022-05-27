1d85793d497a6a02790ffa68ffd056d0bfaacc18
fs-anvr <94833422+fs-anvr@users.noreply.github.com>
2022-05-27 22:14:52 +1000

    Create 1-preschool sorting.cpp

---
+++ CATS/semester_II/1-preschool sorting.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+    ifstream fin ("input.txt");
+    ofstream fout ("output.txt");
+    string str;
+    fin >> str;
+    for (int i = 1; i < str.size(); ++i)
+        if (str[i-1] < 91 && str[i] > 90)
+        {
+            fout << "NO";
+            return 0;
+        }
+    fout << "YES";
+    fin.close();
+    fout.close();
+    return 0;
+}
